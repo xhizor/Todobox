d6c05b1b732b2cecd7806de0f5aa6a0d60cab567
Mateo Mlikota <mateo_mlikota@outlook.com>
2020-01-27 20:48:26 +0100

    Added task component

---
+++ src/components/partials/Task.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="todo-item cursor-pointer unselectable d-flex space-between align-center">
+      <div class="checkbox">
+        <input type="checkbox" class="checkbox-input" :id="task.id" :checked="task.done" 
+          :disabled="task.done" @click="changeStatus">
+        <label :for="task.id" class="checkbox-label" :class="task.done ? 'done': ''"> 
+          {{ task.name }}
+        </label>
+      </div>
+      <div v-if="!task.done">
+        <i class="material-icons show-more">
+          more_vert
+        </i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Task',
+    props: {
+      task: Object
+    },
+    methods: {
+      changeStatus() {
+        this.task.done = true;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/scss/task';  
+</style>
