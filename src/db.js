59df7323c00ef0265fffbc8f4fdc9ece4db9f698
Mateo Mlikota <mateo_mlikota@outlook.com>
2020-01-28 22:06:45 +0100

    Added IndexedDB config

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueIdb from 'vue-idb'
+
+Vue.use(VueIdb)
+
+export default new VueIdb({
+  database: 'todobox',
+  schemas: [{
+    task: '++id, name, status'
+  }]
+})
