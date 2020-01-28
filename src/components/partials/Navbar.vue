b08fe3c72a3b0c8f62a5b3b117b4b42f527d59d7
Mateo Mlikota <mateo_mlikota@outlook.com>
2020-01-28 19:05:58 +0100

    Added logout box

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -6,21 +6,49 @@
         Todobox
       </a>
     </div>
-    <div class="user-box d-flex">
+    <div class="user-box d-flex align-center">
       <span class="user-box-username">
         Username
       </span>
-      <a href="#" class="user-box-avatar">
+      <div class="user-box-avatar position-relative" @click="toggleLogoutBox">
         <img src="http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png" 
-          class="d-flex img" alt="Avatar logo" draggable="false">
-      </a>
+          class="d-flex img" :class="{ 'img-focus': showLogoutBox }" alt="Avatar logo"
+          draggable="false">
+        <div v-if="showLogoutBox" 
+          class="logout-box d-flex align-center has-border-radius"
+          @click="logout">
+          <div class="logout-box-wrapper d-flex space-between align-center">
+            <i class="material-icons logout-box-icon">
+              exit_to_app
+            </i>
+            <span class="logout-box-text">
+              Log out
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>  
 </template>
 
 <script>
   export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+      return {
+        showLogoutBox: false
+      }
+    },
+    methods: {
+      toggleLogoutBox() {
+        this.showLogoutBox = !this.showLogoutBox;
+      },
+      logout() {
+        this.$router.push({ 
+          name: 'Login' 
+        });
+      }
+    }
   }
 </script>
 
