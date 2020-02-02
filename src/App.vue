39e43305141c9ebfeef385f3c96efb4b4065ba0e
Mateo Mlikota <mateo_mlikota@outlook.com>
2020-02-02 16:38:24 +0100

    Added width and height properties

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,14 @@ export default {
     margin: 0;
     box-sizing: border-box;
   }
-  body {
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+  #app {
     background-color:#f1f1f1;
     font-family: 'Roboto';
+    width: 100%;
+    height: 100%;
   }
 </style>
