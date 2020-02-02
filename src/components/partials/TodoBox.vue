7fed7ea8a2b4f918be2d30739eb3cbd8fcf6401e
Mateo Mlikota <mateo_mlikota@outlook.com>
2020-02-02 17:01:07 +0100

    Changed done to status variable, implemented draggable functionality

--- src/components/partials/TodoBox.vue
+++ src/components/partials/TodoBox.vue
@@ -2,33 +2,99 @@
   <div class="box has-border-radius">
     <div class="box-header d-flex space-between">
       <span class="box-header-title">
-        {{ !done ? 'To do': 'Done' }}
+        {{ !status ? 'To do': 'Done' }}
       </span>
-      <a class="cursor-pointer">
+      <a class="cursor-pointer"
+         @click="handleTaskAction"
+         :title="!status ? 'Add a task': 'Delete all done tasks'">
         <i class="material-icons box-header-icon unselectable">
-          {{ !done ? 'add_box': 'delete_sweep' }}
+          {{ !status ? 'add_box': 'delete_sweep' }}
         </i>
       </a>
     </div>
-      <Task v-for="task in taskList" :task="task" :key="task.id"/>
+    <draggable
+      v-model="tasks"
+      @change="onTaskDrop"
+      group="tasks">
+      <Task
+        v-for="task in tasks"
+        :task="task"
+        :key="task.id"/>
+      </draggable>
   </div>
 </template>
 
 <script>
   import Task from '../partials/Task';
+  import draggable from 'vuedraggable'
 
   export default {
     name: 'TodoBox',
     props: {
-      done: Boolean,
-      taskList: Array
+      status: Number,
+      taskList: Array,
+    },
+    data() {
+      return {
+        tasks: this.taskList
+      }
+    },
+    watch: {
+      taskList() {
+        this.tasks = this.taskList;
+      }
     },
     components: {
-      Task
+      Task,
+      draggable
+    },
+    methods: {
+      updateTaskList() {
+        this.$db.task.toArray().then(tasks =>
+          this.$store.state.taskList = tasks
+        );
+      },
+      handleTaskAction() {
+        if (!this.status) {
+          const isEmptyTaskExist = this.$store.state.taskList.filter(
+            task => !task.id
+          );
+          if (!isEmptyTaskExist.length) {
+            this.initTask();
+          }
+          return;
+        }
+
+        if (this.tasks.length) {
+          this.removeDoneTasks();
+        }
+      },
+      initTask() {
+        this.$store.state.taskList.push({
+          id: 0,
+          name: '',
+          isEditable: true
+        });
+      },
+      removeDoneTasks() {
+        this.$db.task.where('status').equals(1).delete().then(() =>
+          this.updateTaskList()
+        ).catch(alert);
+      },
+      onTaskDrop(event) {
+        if (event.added) {
+          const { id, status } = event.added.element;
+          this.$db.task.update(id, {
+            status: status === 0 ? 1: 0
+          }).then(() => {
+            this.updateTaskList();
+          });
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  @import '../../assets/scss/todo-box';  
+  @import '../../assets/scss/todo-box';
 </style>
